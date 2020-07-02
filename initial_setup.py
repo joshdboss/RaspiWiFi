138920d1346a8a47deb0d4e14225f863850e8cdf
joshdboss <newbie856@gmail.com>
2020-07-02 16:26:24 +0200

    added reboot after raspiwifi
    or else everything bugs

--- initial_setup.py
+++ initial_setup.py
@@ -60,3 +60,7 @@ print("#####################################")
 print()
 print()
 print("Initial setup is complete. A reboot is required to start in WiFi configuration mode...")
+reboot_ans = input("Would you like to do that now? [y/N]: ")
+
+if reboot_ans.lower() == 'y':
+	os.system('reboot')
